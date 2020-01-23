8c8dc7df40ed648d85457dfa4557c07b4fe39e10
isaacmm110 <60229915+isaacmm110@users.noreply.github.com>
2020-01-23 10:16:17 -0600

    First Commit

---
+++ Calculadora++.py
@@ -0,0 +1,15 @@
+
+num1 = float(input("Enter a number: "))
+op = input("Enter opeator: ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "x":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("Elefante")
