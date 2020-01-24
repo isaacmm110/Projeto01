d970a615fa4e7d6287c6fb32b8225bd2d252de10
isaacmm110 <60229915+isaacmm110@users.noreply.github.com>
2020-01-24 12:56:24 -0600

    Translator++
    Now you can choose what letter you want to change in your sentence

--- Translator.py
+++ Translator.py
@@ -1,12 +1,20 @@
 
+
 def translate(phrase):
+    letter1 = input("Enter the letter which all the vocals will turn on: ")
+    letter2 = input("Enter the letter which all the r will be changed: ")
+    translation2 = ""
     translation = ""
     for letter in phrase:
         if letter in "AEIOUaeiou":
-            translation = translation + "u"
+            translation = translation + letter1
         else:
             translation = translation + letter
-    return translation
+    for letter in translation:
+        if letter in "Rr":
+            translation2 = translation2 + letter2
+        else:
+            translation2 = translation2 + letter
+    return translation2
 
-
-print(translate(input(" ")))
+print(translate(input("")))
