aec131aa5eb6ebdd38b6b08e09a5d1b1b47079e4
isaacmm110 <60229915+isaacmm110@users.noreply.github.com>
2020-01-23 19:28:37 -0600

    Revert "Validating division by 0"

--- Calculadora++.py
+++ Calculadora++.py
@@ -10,9 +10,6 @@ elif op == "-":
 elif op == "x":
     print(num1 * num2)
 elif op == "/":
-    while (num2 == 0):
-        num2 = float(input("Enter a valid second number: "))
     print(num1 / num2)
-
 else:
     print("Elefante")
